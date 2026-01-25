2e4d9bd02e38b68eaddd7139d6f1ba94f3f9b81c
Kimchoho <flexmania@gmail.com>
2026-01-26 00:03:41 +0900

    chore: reset portal directory (preserved .docs and .agent)

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function RootPage() {
-  // Logic to determine if user is admin or app user can go here
-  // For now, default to apps dashboard
-  redirect("/apps/dashboard");
-}
